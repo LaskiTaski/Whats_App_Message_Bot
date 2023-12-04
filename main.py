3b6824be1686365ebde97013e0294cfd247f17d8
Krasnikov Artem <dgj.vap.11@mail.ru>
2023-12-04 20:11:05 +0300

    Add Fixed XPath and Random time

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from random import randint
 import openpyxl
 import json
+import time
 
 wookbook = openpyxl.load_workbook("number_list.xlsx")
 worksheet = wookbook.active
 
-input_number = '/html/body/div[1]/div/div/div[3]/div[1]/span/div/span/div/div[1]/div[2]/div[2]/div/div[1]/p'
-input_message = '/html/body/div[1]/div/div/div[5]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]'
+input_number = '//*[@id="app"]/div/div[2]/div[2]/div[1]/span/div/span/div/div[1]/div[2]/div[2]/div/div[1]/p'
+input_message = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p'
 
-btn_new_chat = '/html/body/div[1]/div/div/div[4]/header/div[2]/div/span/div[3]/div/span'
-btn_user_account = '/html/body/div[1]/div/div/div[3]/div[1]/span/div/span/div/div[2]/div[2]/div[2]'
-btn_send_message = '/html/body/div[1]/div/div/div[5]/div/footer/div[1]/div/span[2]/div/div[2]/div[2]'
-btn_back = '/html/body/div[1]/div/div/div[3]/div[1]/span/div/span/div/header/div/div[1]/div'
+btn_new_chat = '//*[@id="app"]/div/div[2]/div[3]/header/div[2]/div/span/div[3]/div'
+btn_user_account = '//*[@id="app"]/div/div[2]/div[2]/div[1]/span/div/span/div/div[2]/div[2]'
+btn_send_message = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button'
+btn_back = '//*[@id="app"]/div/div[2]/div[2]/div[1]/span/div/span/div/div[1]/div[2]/button'
 
 def load_numbers(): #Считывание всех номеров из xlsx
     list_number = []
@@ -25,23 +26,23 @@ def load_numbers(): #Считывание всех номеров из xlsx
 
 def click_new_chat():# Нажатие на кнопку новый чат
     browser.find_element(By.XPATH, btn_new_chat).click()
-    time.sleep(7)
+    time.sleep(randint(6, 11))
 
 def entering_number(number):# Вставляем номер телефона
     browser.find_element(By.XPATH, input_number).send_keys(number)
-    time.sleep(6)
+    time.sleep(randint(6, 11))
 
 def click_user_account():# Выбираем аккаунт "кому отправить"
     browser.find_element(By.XPATH, btn_user_account).click()
-    time.sleep(4)
+    time.sleep(randint(6, 11))
 
 def entering_message(message): # Пишем текст
     browser.find_element(By.XPATH, input_message).send_keys(message)
-    time.sleep(8)
+    time.sleep(randint(6, 11))
 
 def click_send_message(): # Нажатие на кнопку отправить
     browser.find_element(By.XPATH, btn_send_message).click()
-    time.sleep(5)
+    time.sleep(randint(6, 11))
 
 def click_back(): # Вернуться назад если пользователь не найден
     browser.find_element(By.XPATH, btn_back).click()
