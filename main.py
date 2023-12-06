6aa6d0ba0af837672965dcbf86d02d157be56617
Krasnikov Artem <dgj.vap.11@mail.ru>
2023-12-06 19:33:13 +0300

    Fixed XPath

--- main.py
+++ main.py
@@ -1,58 +1,45 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium.webdriver.support.ui import WebDriverWait
 from time import sleep
 from random import randint
 import openpyxl
 import json
 
 
-
 options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized")
-options.add_argument('user-data-dir=C:\\Chrome\\User Data') # УКАЖИТЕ ПУТЬ ГДЕ ЛЕЖИТ ВАШ ФАЙЛ. Советую создать отдельную папку.
+options.add_argument('user-data-dir=C:Google\\Chrome\\User Data') # УКАЖИТЕ ПУТЬ ГДЕ ЛЕЖИТ ВАШ ФАЙЛ. Советую создать отдельную папку.
 
+workbook = openpyxl.load_workbook("number_list.xlsx")
+worksheet = workbook.active
 
-
-wookbook = openpyxl.load_workbook("number_list.xlsx")
-worksheet = wookbook.active
-
-input_number = '//*[@id="app"]/div/div[2]/div[2]/div[1]/span/div/span/div/div[1]/div[2]/div[2]/div/div[1]/p'
-input_message = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p'
-
-btn_new_chat = '//*[@id="app"]/div/div[2]/div[3]/header/div[2]/div/span/div[3]/div'
-btn_user_account = '//*[@id="app"]/div/div[2]/div[2]/div[1]/span/div/span/div/div[2]/div[2]'
-btn_send_message = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button'
-btn_back = '//*[@id="app"]/div/div[2]/div[2]/div[1]/span/div/span/div/header/div/div[1]/div/span'
 
 def load_numbers(): #Считывание всех номеров из xlsx
     list_number = [col[i].value for i in range(1, worksheet.max_row) for col in worksheet.iter_cols(1, worksheet.max_column) if col[i].value]
     return list_number
 
 def click_new_chat():# Нажатие на кнопку новый чат
-    browser.find_element(By.XPATH, btn_new_chat).click()
+    browser.find_element(By.XPATH, '//*[@aria-label="Новый чат"]').click()
     sleep(randint(6, 11))
 
 def entering_number(number):# Вставляем номер телефона
-    browser.find_element(By.XPATH, input_number).send_keys(number)
+    browser.find_element(By.XPATH, '//*[@title="Текстовое поле поиска"]').send_keys(number)
     sleep(randint(6, 11))
 
 def click_user_account():# Выбираем аккаунт "кому отправить"
-    browser.find_element(By.XPATH, btn_user_account).click()
+    browser.find_element(By.XPATH, '//*[@tabindex="0" and @class="_199zF _3j691" and @role="button"]').click()
     sleep(randint(6, 11))
 
 def entering_message(message): # Пишем текст
-    browser.find_element(By.XPATH, input_message).send_keys(message)
+    browser.find_element(By.XPATH, '//*[@title="Введите сообщение"]').send_keys(message)
     sleep(randint(6, 11))
 
 def click_send_message(): # Нажатие на кнопку отправить
-    browser.find_element(By.XPATH, btn_send_message).click()
+    browser.find_element(By.XPATH, '//*[@aria-label="Отправить"]').click()
     sleep(randint(6, 11))
 
 def click_back(): # Вернуться назад если пользователь не найден
-    browser.find_element(By.XPATH, btn_back).click()
+    browser.find_element(By.XPATH, '//*[@aria-label="Назад"]').click()
     sleep(2)
 
 result_json = {}
